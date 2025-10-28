49b71040dce7275c3f428be76467efce3cc5ee1e
Anirudh <vennapusaani1629@gmail.com>
2025-10-29 02:10:22 +0530

    frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+return (
+<div className="App">
+<header className="App-header">
+<h1>🚀 Welcome to Your React App</h1>
+<p>Edit <code>src/App.jsx</code> and save to reload.</p>
+<a className="App-link" href="https://react.dev" target="_blank" rel="noopener noreferrer" >
+Learn React
+</a>
+</header>
+</div>
+);
+}
+
+export default App;
